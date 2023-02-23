1e181df11f0d4daf799cdc683d895fb4e313499c
Ankita <ankita.b.bhatnagar@capgemini.com>
2023-02-23 22:18:18 +0200

    content type check added

--- expressJsonParser.js
+++ expressJsonParser.js
@@ -1,17 +1,26 @@
 function jsonBodyParser(req, res, next) {
-    let data = '';
-  
-    req.on('data', (dataChunk) => {
-      data += dataChunk.toString();
-    });
-  
-    req.on('end', () => {
-      try {
-        req.body = JSON.parse(data);
-        next();
-      } catch (err) {
-        res.status(400).json({ message: 'Invalid JSON data' });
-      }
-    });
+  let data = "";
+  let contentType = req.headers["content-type"] || "application/json";
+  let skipParsing = false;
+  if (contentType != "application/json") {
+    skipParsing = true;
   }
-  module.exports = jsonBodyParser;
+  req.on("data", (dataChunk) => {
+    if(skipParsing)
+      return;
+    data += dataChunk.toString();
+  });
+
+  req.on("end", () => {
+    try {
+      console.log(data);
+      if(skipParsing)
+      throw new Error();
+      req.body = JSON.parse(data);
+      next();
+    } catch (err) {
+      res.status(400).json({ message: "Invalid JSON data" });
+    }
+  });
+}
+module.exports = jsonBodyParser;
